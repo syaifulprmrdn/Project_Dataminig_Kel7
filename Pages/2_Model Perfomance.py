5b3029fe45e5e2d8275d849ed28dc86292d2a5bf
syaifulprmrdn <syaifulprimordian@gmail.com>
2025-06-04 17:53:37 +0700

    Update 2_Model Perfomance.py

--- Pages/2_Model Perfomance.py
+++ Pages/2_Model Perfomance.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Prediction", page_icon="📈")
 st.header("Predictions")
 
 #Load Dataset
-df = pd.read_csv("model/iris.csv")
+df = pd.read_csv("Model/iris.csv")
 dataset = pd.read_csv('https://gist.githubusercontent.com/netj/8836201/raw/6f9306ad21398ea43cba4f7d537619d0e07d5ae3/iris.csv')
 
 #Tampilkan Dataframe
