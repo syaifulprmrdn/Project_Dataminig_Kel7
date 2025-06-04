83d73b408116e08de0b0e7a06af413e367929572
syaifulprmrdn <syaifulprimordian@gmail.com>
2025-06-04 19:55:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.title("🎈 Selamat datang di Aplikasi Dashboard Data Life Expectancy")
 st.write("Aplikasi ini memvisualisasikan data Life Expectancy.")
 
 # Load Dataset
-df = pd.read_csv("Model/Life Expectancy.csv")
+df = pd.read_csv("Model/Life Expectancy Data.csv")
 
 # Tampilkan dataset dan kolom
 st.subheader("📁 Dataset Life Expectancy")
