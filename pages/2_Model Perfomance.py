97314669cbddaf0237b812a5cfc77f4cf4013e64
syaifulprmrdn <syaifulprimordian@gmail.com>
2025-06-04 20:43:30 +0700

    Update 2_Model Perfomance.py

--- pages/2_Model Perfomance.py
+++ pages/2_Model Perfomance.py
@@ -14,7 +14,7 @@ st.title("🚀 Model Performance Dashboard: Life Expectancy")
 # Load Dataset
 df = pd.read_csv("Model/Life_Expectancy_Data.csv")
 st.subheader("📁 Dataset Life Expectancy")
-st.dataframe(df)
+st.dataframe(dataset)
 
 # Preprocessing
 st.subheader("⚙️ Preprocessing Data")
